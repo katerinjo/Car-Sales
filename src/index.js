0de0a5ae0e1ef603722dc3136336524f1bb668b7
Catherine Stewart <catherine.c.stwrt@gmail.com>
2019-08-14 14:58:47 -0700

    initial redux setup

--- src/index.js
+++ src/index.js
@@ -5,5 +5,26 @@ import App from './App';
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducer from './reducer';
+
+const store = createStore(reducer, {
+  additionalPrice: 0,
+  car: {
+    price: 26395,
+    name: '2019 Ford Mustang',
+    image:
+      'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
+    features: []
+  },
+  store: [
+    { id: 1, name: 'V-6 engine', price: 1500 },
+    { id: 2, name: 'Racing detail package', price: 1500 },
+    { id: 3, name: 'Premium sound system', price: 500 },
+    { id: 4, name: 'Rear spoiler', price: 250 }
+  ]
+});
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Provider store={store}><App /></Provider>, rootElement);
